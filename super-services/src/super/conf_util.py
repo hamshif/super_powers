6907c453833339c515c7161d658967085860b7b0
gideon <hamshif@gmail.com>
2026-01-18 12:32:19 +0200

    inferring project name

--- super-services/src/super/conf_util.py
+++ super-services/src/super/conf_util.py
@@ -8,6 +8,7 @@ from super.config import get_app_conf
 def main() -> None:
     conf = get_app_conf(app="generate_powers")
     assert conf.super_hero == "Buggs Bunny", "app config should override project super_hero"
+    assert conf.get("garbage") == "zevel adom", "developer config should be included"
     print(HOCONConverter.to_hocon(conf))
 
 
